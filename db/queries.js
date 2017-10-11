7efbfd21bf249158c0b65b795f7738cd94b294d0
Monica Williams <monicaestellaw@gmail.com>
2017-10-10 17:57:12 -0700

    we are rendering all the data from the database correctly

--- db/queries.js
+++ db/queries.js
@@ -4,6 +4,11 @@ const createTodo = name => {
   return db.any('INSERT INTO tasks(name, completed) VALUES($1, false) RETURNING (name)', [name])
 }
 
+const getAllTodos = () => {
+  return db.any('SELECT * FROM tasks')
+}
+
 module.exports = {
-  createTodo
+  createTodo,
+  getAllTodos
 }
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,26 @@
 const express = require( 'express' );
 const app = express.Router();
 
-const { createTodo } = require('../db/queries')
+const { createTodo, getAllTodos } = require('../db/queries')
 
 /* GET home page. */
 app.get( '/', function( request, response ) {
 
-  response.render( 'index', { title: 'Our app'});
+  getAllTodos()
+    .then( data => {
+      console.log('data', data)
+      return data
+    })
+    .then( data => response.render( 'index', { allTodos: data }))
+    .catch( error => response.render( 'error', { error }))
 });
 
-app.post('/newitem', function (request, response) {
+app.post( '/newitem', function ( request, response ) {
   const { name } = request.body
 
-  createTodo(name)
-    .then( data => response.redirect('/'))
-    .catch( error => response.render('error', { error }))
+  createTodo( name )
+    .then( data => response.redirect( '/' ))
+    .catch( error => response.render( 'error', { error }))
 })
 
 module.exports = app;
