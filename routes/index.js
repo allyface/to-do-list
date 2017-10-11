75a5494cfd9b92dfd11eebff3af4b4684b4d7809
Monica Williams <monicaestellaw@gmail.com>
2017-10-10 17:28:22 -0700

    we are correctly posting to the database and redirecting to the homepage

--- routes/index.js
+++ routes/index.js
@@ -5,16 +5,16 @@ const { createTodo } = require('../db/queries')
 
 /* GET home page. */
 app.get( '/', function( request, response ) {
-  
-  response.render( 'index', { title: name });
+
+  response.render( 'index', { title: 'Our app'});
 });
 
 app.post('/newitem', function (request, response) {
   const { name } = request.body
 
   createTodo(name)
-    .then( data => response.status( 201 ).redirect('/'))
-    .catch(error => response.render('error', { error }))
+    .then( data => response.redirect('/'))
+    .catch( error => response.render('error', { error }))
 })
 
 module.exports = app;
